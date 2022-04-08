cc8f7bf6c5bab3fde05d82dfa08dd2b6e36cc122
lichenj <lichenj@andrew.cmu.edu>
2022-04-08 15:38:35 +0000

    update sql file

--- cmudb/extensions/qcache/qcache--0.0.1.sql
+++ cmudb/extensions/qcache/qcache--0.0.1.sql
@@ -1,8 +1,1 @@
-drop table if exists pg_catalog.pg_qcache;
-create table pg_catalog.pg_qcache(
-    queryid bigint,
-    dbid oid,
-    procid int32,
-    timestamp int64,
-    primary key(queryid, dbid, procid)
-);
+-- Do nothing here.
